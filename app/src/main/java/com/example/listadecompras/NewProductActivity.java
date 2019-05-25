30204b36a6ca6c0a4b6bfe2c3b7c25f4bb3f34d5
Jander <janderrv26@gmail.com>
2019-05-25 16:03:05 -0400

    Criação de tela de criar produto

---
+++ app/src/main/java/com/example/listadecompras/NewProductActivity.java
@@ -0,0 +1,13 @@
+package com.example.listadecompras;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_product);
+    }
+}
